b28e1e3b7e3eb9cb93d8a13e01629f35dc31ac89
wgnico <nicoburgos1989@gmail.com>
2024-09-22 15:47:31 -0300

    commit para sulucionar error

--- src/app/domains/shared/header/header.component.ts
+++ src/app/domains/shared/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NzMenuModule,NzIconModule],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
