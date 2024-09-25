e6160d085e744e6cbdf34d0d2f79a330f9c1f641
wgnico <nicoburgos1989@gmail.com>
2024-09-24 23:06:23 -0300

    se modificia el css de inicio

--- src/app/domains/pages/inicio/inicio.component.ts
+++ src/app/domains/pages/inicio/inicio.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzImageModule } from 'ng-zorro-antd/image';
-
+import { NzGridModule } from 'ng-zorro-antd/grid';
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [NzImageModule, NzDividerModule],
+  imports: [NzImageModule, NzDividerModule, NzCardModule,NzGridModule ],
   templateUrl: './inicio.component.html'
 })
 export default class InicioComponent {
+
+  
 
 }
