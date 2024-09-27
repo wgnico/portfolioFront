27e80606f28d88992970570b04fe739f5a631061
wgnico <nicoburgos1989@gmail.com>
2024-09-27 20:33:20 -0300

    se agrega forma a los cards

--- src/app/domains/pages/inicio/inicio.component.ts
+++ src/app/domains/pages/inicio/inicio.component.ts
@@ -3,10 +3,13 @@ import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzImageModule } from 'ng-zorro-antd/image';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzListModule } from 'ng-zorro-antd/list';
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [NzImageModule, NzDividerModule, NzCardModule,NzGridModule ],
+  imports: [NzImageModule, NzDividerModule, NzCardModule,NzGridModule,NzTabsModule, NzIconModule, NzListModule],
   templateUrl: './inicio.component.html'
 })
 export default class InicioComponent {
