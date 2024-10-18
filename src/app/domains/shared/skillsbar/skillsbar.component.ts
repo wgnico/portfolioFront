0971ce0ec76759387308c4b748db1673cd559181
wgnico <nicoburgos1989@gmail.com>
2024-10-18 08:13:35 -0300

    se agrega servicio proyectos

---
+++ src/app/domains/shared/skillsbar/skillsbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skillsbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './skillsbar.component.html'
+})
+export class SkillsbarComponent {
+
+}
